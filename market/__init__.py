491b34846bb158c719efecb5b28cda5ca330b741
Ayush aggarwal <aayushaggarwal348@gmail.com>
2025-08-05 21:47:34 +0530

    Creating forms from flask

--- market/__init__.py
+++ market/__init__.py
@@ -2,7 +2,10 @@ from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__, instance_relative_config=True)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'b303366d7cc1afd485c2274f'
 db = SQLAlchemy(app)
+from market import routes
+
 
 # @app.route('/')
 # def hello_world():
@@ -11,5 +14,3 @@ db = SQLAlchemy(app)
 # @app.route('/about/<username>')
 # def about(username):
 #     return f'<h1> This is a about page of {username}</h1>'
-
-from market import routes
